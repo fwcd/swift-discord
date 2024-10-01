fb02932ae2984cb12286a993ad518a1df54d02b1
fwcd <fwcd@fwcd.dev>
2024-10-02 01:34:38 +0200

    Add some more interaction response types

--- Sources/Discord/Interaction/DiscordInteractionResponse.swift
+++ Sources/Discord/Interaction/DiscordInteractionResponse.swift
@@ -30,11 +30,30 @@ public enum DiscordInteractionResponseType: Int, Encodable {
     @available(*, deprecated, message: "This type no longer seems to be supported")
     case channelMessage = 3
 
-    /// Respond with a message, showing the user's input
+    /// Respond to an interaction with a message
     case channelMessageWithSource = 4
 
-    /// Ack a command without sending a message, showing the user's input
-    case ackWithSource = 5
+    /// ACK an interaction and edit a response later, the user sees a loading state
+    case deferredChannelMessageWithSource = 5
+
+    /// For components, ACK an interaction and edit the original message later; the user does not see a loading state
+    case deferredUpdateMessage = 6
+
+    /// For components, edit the message the component was attached to
+    case updateMessage = 7
+
+    /// Respond to an autocomplete interaction with suggested choices
+    case applicationCommandAutocompleteResult = 8
+
+    /// Respond to an interaction with a popup modal. Not available for `MODAL_SUBMIT` and `PING` interactions.
+    case modal = 9
+
+    /// Respond to an interaction with an upgrade button, only available for apps with monetization enabled
+    @available(*, deprecated, message: "Deprecated in the official API docs")
+    case premiumRequired = 10
+
+    /// Launch the Activity associated with the app. Only available for apps with Activities enabled
+    case launchActivity = 12
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
